da3f31eac750f889faf7a01426c57eae8bdda566
jasonchan <jsoanchenzehui@gmail.com>
2019-09-02 17:27:03 +0800

    d

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,50 @@
 module.exports = {
+  base: '/',
   title: '前端日常积累',
   description: '记录点滴',
-  base: '/',
+  head: [
+    ['link', {rel: 'icon', href: '/logo.png'}]
+  ],
+  host: '0.0.0.0',
+  port: 9527,
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
   themeConfig: {
     activeHeaderLinks: false, // 默认值：true
     displayAllHeaders: true, // 默认值：false
+    sidebarDepth: 0,
     lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
     sidebar: [
+      // ['url', '标题'],
       ['/', '简介'],
       {
-        title: "vue",
+        title: 'javascript',
         collapsable: false,
-        children:[
-          ['/vue/Vue 定义全局函数.md', 'Vue 定义全局函数'],
+        children: [
+          // ['url', '标题'],
+          ['/javascript/', 'javascript相关'],
+        ]
+      },
+      {
+        title: 'code',
+        collapsable: false,
+        children: [
+          ['/code/', 'code相关'],
+        ]
+      },
+      {
+        title: 'vue',
+        collapsable: false,
+        children: [
+          ['/vue/', 'vue相关'],
+        ]
+      },
+      {
+        title: 'webpack',
+        collapsable: false,
+        children: [
+          ['/webpack/', 'webpack相关'],
         ]
       },
     ]
